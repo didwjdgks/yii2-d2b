b1fe6c94a1ff9a2b6aba47d395c380638701793e
didwjdgks <yjh@djcp.net>
2016-07-18 15:53:19 +0900

    edit

--- controllers/WatchController.php
+++ controllers/WatchController.php
@@ -7,6 +7,8 @@ use d2b\WatchEvent;
 
 use d2b\watchers\HI_PEB_Announce_Lst;
 use d2b\watchers\HI_PEB_OpenNego_Lst;
+use d2b\watchers\HI_PDB_Announce_Lst;
+use d2b\watchers\HI_PDB_OpenNego_Lst;
 
 use d2b\watchers\HI_PEB_BidResult_Lst;
 use d2b\watchers\HI_PEB_OpenNegoResult_Lst;
@@ -18,6 +20,12 @@ use d2b\models\BidKey;
 class WatchController extends \yii\console\Controller
 {
   public function actionBid(){
+    $pdb=new HI_PDB_Announce_Lst;
+    $pdb->on(WatchEvent::EVENT_ROW,[$this,'onBidPdbRow']);
+
+    $pdb2=new HI_PDB_OpenNego_Lst;
+    $pdb2->on(WatchEvent::EVENT_ROW,[$this,'onBidPdb2Row']);
+
     $peb=new HI_PEB_Announce_Lst;
     $peb->on(WatchEvent::EVENT_ROW,[$this,'onBidPebRow']);
 
@@ -26,10 +34,14 @@ class WatchController extends \yii\console\Controller
 
     while(true){
       try {
+        $this->stdout("==국내조달 공개협상계획==\n",Console::FG_YELLOW);
+        $pdb2->watch();
         $this->stdout("==시설공사 입찰공고==\n",Console::FG_YELLOW);
         $peb->watch();
         $this->stdout("==시설공사 공개협상계획==\n",Console::FG_YELLOW);
         $peb2->watch();
+        $this->stdout("==국내조달 경쟁입찰공고==\n",Console::FG_YELLOW);
+        $pdb->watch();
       }
       catch(\Exception $e){
         $this->stdout($e.PHP_EOL,Console::FG_RED);
@@ -42,6 +54,51 @@ class WatchController extends \yii\console\Controller
       );
       \d2b\Http::sleep();
     }
+  }
+
+  /**
+   * 국내조달 경쟁입찰공고 row 
+   */
+  public function onBidPdbRow($event){
+    $row=$event->row;
+    $out[]="[D2B] %g{$row['notinum']}%n %c{$row['constno']}%n {$row['constnm']} [{$row['status']}]";
+
+    $bidkey=BidKey::find()->where([
+      'whereis'=>'10',
+      'notinum'=>$row['notinum'],
+      'notinum_ex'=>$row['constno'],
+      'state'=>['Y','N'],
+    ])->orderBy('bidid desc')->limit(1)->one();
+    if($bidkey===null){
+      if($row['contract']!='지명경쟁'){
+        $out[]="%rNEW%n";
+      }
+    }else $out[]="({$bidkey->bidproc})";
+    $this->stdout(Console::renderColoredString(join(' ',$out)).PHP_EOL);
+  }
+
+  /**
+   * 국내입찰 공개수의협상 공고 row
+   */
+  public function onBidPdb2Row($event){
+    $row=$event->row;
+    $out[]="[D2B] %c{$row['constno']}%n {$row['constnm']} [{$row['bidproc']}]";
+
+    $bidkey=BidKey::find()->where([
+      'whereis'=>'10',
+      'notinum'=>$row['hidAnmtNumb'].'-'.$row['degree'],
+      'notinum_ex'=>$row['constno'],
+      'state'=>['Y','N'],
+    ])->orderBy('bidid desc')->limit(1)->one();
+    if($bidkey===null){
+      $out[]="%rNEW%n";
+    }else{
+      $out[]="({$bidkey->bidproc})";
+      if($row['bidproc']=='공개협상취소' and $bidkey->bidproc!=='C'){
+        $out[]="%r취소%n";
+      }
+    }
+    $this->stdout(Console::renderColoredString(join(' ',$out)).PHP_EOL);
   }
 
   /**
